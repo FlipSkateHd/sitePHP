42e158fabc3dfeef2bf3a25d619cf8f8fec3e99e
Flip <117127775+FlipSkateHd@users.noreply.github.com>
2024-09-06 12:38:41 -0300

    commit inicial

---
+++ sitePHP/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>SK-T1</title>
+</head>
+<body>
+    <center>
+    <h1>Sobre o Time T1:</h1>  
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/T1_esports_logo.svg/200px-T1_esports_logo.svg.png" id="fotot1/">
+    </center>
+    <br>
+    <p>É um time de E-sports do jogo League of Legends, conhecido por seu famoso integrante Faker, um dos melhores jogadores do mundo.</p>  
+    <p>Originalmente criado em 2002 pelo jogador de Star Craft Lim "BoxeR", e mais tarde em 2004 a operadora SK Telecom começou a patrocinar a equipe criando a SK Telecom T1.</p>
+    
+    <p>A equipe de League of Legends da T1 tem um recorde de quatro títulos do Campeonato Mundial de League of Legends (2013, 2015, 2016 e 2023) e dez títulos de League of Legends Champions Korea</p>
+    
+</body>
+</html>
